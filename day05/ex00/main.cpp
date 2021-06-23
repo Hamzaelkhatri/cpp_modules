55cbb624c31bbb6aef6a1956e27913789121b90b
hamza <hamzaelkhatri@gmail.com>
2021-06-23 16:08:45 +0100

    finish ex00

---
+++ day05/ex00/main.cpp
@@ -0,0 +1,20 @@
+#include "Bureaucrat.hpp"
+
+int main()
+{
+    try
+    {
+        Bureaucrat b("EX01", 100);
+        Bureaucrat b1("EX02", 150);
+        // b1.decrGrade();
+        Bureaucrat b2("EX03", 1);
+        Bureaucrat b3("EX04", 0);
+        Bureaucrat b4("EX05", 121);
+        std::cout << b << b1 << b2;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    return (0);
+}
