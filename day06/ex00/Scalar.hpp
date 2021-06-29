f6f7deb3ad7d8552aef778ac3d3b0610ed44d95e
hamza <hamzaelkhatri@gmail.com>
2021-06-29 23:15:40 +0100

    init d6

---
+++ day06/ex00/Scalar.hpp
@@ -0,0 +1,23 @@
+#ifndef SCALAR_HPP
+#define SCALAR_HPP
+#include <iomanip>
+#include <string>
+#include <iostream>
+class Scalar
+{
+private:
+    /* data */
+public:
+    Scalar(/* args */);
+    ~Scalar();
+};
+
+Scalar::Scalar(/* args */)
+{
+}
+
+Scalar::~Scalar()
+{
+}
+
+#endif
---
+++ day06/ex00/main.cpp
@@ -0,0 +1,14 @@
+#include <iomanip>
+#include <string>
+#include <iostream>
+
+int main(int ac, char **ag)
+{
+    if (ac != 2)
+    {
+        std::cerr << "Arguement two not found" << std::endl;
+        return (1);
+    }
+
+    return (0);
+}
